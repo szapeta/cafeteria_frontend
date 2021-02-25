958b632fbc12fff4a64041910dc11ba34fc11d41
szapeta <2375894860101@ingenieria.usac.edu.gt>
2021-02-25 00:30:43 -0600

    Modificacion del route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+
+const routes: Routes = [
+{path: 'productos', component: ProductosComponent},
+{path: 'producto/:id', component: ProductoComponent},
+{path: '**', pathMatch:'full', redirectTo: 'productos'}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
